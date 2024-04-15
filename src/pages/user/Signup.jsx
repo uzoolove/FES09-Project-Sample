268fb0878b6e5b2cd0e58dd996e2522d3ea7f9e0
Kilyong Jeong <uzoolove@gmail.com>
2024-04-15 14:45:48 +0900

    🐛 파일 업로드 api 응답 바뀌면서 발생한 오류 수정

--- src/pages/user/Signup.jsx
+++ src/pages/user/Signup.jsx
@@ -35,7 +35,7 @@ function Signup() {
         });
 
         // 서버로부터 응답받은 이미지 이름을 회원 정보에 포함
-        formData.profileImage = fileRes.data.file.name;
+        formData.profileImage = fileRes.data.item[0].name;
       } else {
         // profileImage 속성을 제거
         delete formData.profileImage;

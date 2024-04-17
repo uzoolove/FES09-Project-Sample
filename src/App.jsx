ecac9b1544481823ea05fb2b3a361eb264790b55
Kilyong Jeong <uzoolove@gmail.com>
2024-04-17 10:47:12 +0900

    🐛 이전/다음 버튼으로 페이지 이동시 모달 닫히지 않는 문제 수정
    Modal 컴포넌트를 App.jsx 대신 layout으로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
         </Suspense>
       </RecoilRoot>
       <ReactQueryDevtools initialIsOpen={false} />
-      <Modal />
     </QueryClientProvider>
   );
 }
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,13 +1,24 @@
+import Modal from '@components/Modal';
 import Footer from '@components/layout/Footer';
 import Header from '@components/layout/Header';
-import { Outlet } from 'react-router-dom';
+import useModalStore from '@zustand/modalStore.mjs';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function Layout() {
+  const location = useLocation();
+  const closeModal = useModalStore((state) => state.closeModal);
+
+  useEffect(() => {
+    closeModal();
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Outlet />
       <Footer />
+      <Modal />
     </div>
   );
 }
